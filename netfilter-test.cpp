373ee872ac2f38049fc4ca8e672207b3fed62c1d
Pseung-min <psm345601@naver.com>
2020-08-19 22:06:15 +0900

    netfilter-test

--- netfilter-test.cpp
+++ netfilter-test.cpp
@@ -19,67 +19,16 @@
 
 const unsigned char *malicious_host;
 
-void dump(unsigned char* buf, int size) {
-	int i;
-	for (i = 0; i < size; i++) {
-		if (i % 16 == 0)
-			printf("\n");
-		printf("%02x ", buf[i]);
-	}
-}
-
 /* returns packet id */
 static u_int32_t print_pkt (struct nfq_data *tb)
 {
 	int id = 0;
 	struct nfqnl_msg_packet_hdr *ph;
-	struct nfqnl_msg_packet_hw *hwph;
-	u_int32_t mark,ifi;
-	int ret;
-	unsigned char *data;
 
 	ph = nfq_get_msg_packet_hdr(tb);
 	if (ph) {
 		id = ntohl(ph->packet_id);
-		printf("hw_protocol=0x%04x hook=%u id=%u ",
-			ntohs(ph->hw_protocol), ph->hook, id);
 	}
-
-	hwph = nfq_get_packet_hw(tb);
-	if (hwph) {
-		int i, hlen = ntohs(hwph->hw_addrlen);
-
-		printf("hw_src_addr=");
-		for (i = 0; i < hlen-1; i++)
-			printf("%02x:", hwph->hw_addr[i]);
-		printf("%02x ", hwph->hw_addr[hlen-1]);
-	}
-
-	mark = nfq_get_nfmark(tb);
-	if (mark)
-		printf("mark=%u ", mark);
-
-	ifi = nfq_get_indev(tb);
-	if (ifi)
-		printf("indev=%u ", ifi);
-
-	ifi = nfq_get_outdev(tb);
-	if (ifi)
-		printf("outdev=%u ", ifi);
-	ifi = nfq_get_physindev(tb);
-	if (ifi)
-		printf("physindev=%u ", ifi);
-
-	ifi = nfq_get_physoutdev(tb);
-	if (ifi)
-		printf("physoutdev=%u ", ifi);
-
-	ret = nfq_get_payload(tb, &data);
-	if (ret >= 0) {
-		printf("payload_len=%d ", ret);
-		// dump(data, ret);
-	}
-	fputc('\n', stdout);
 
 	return id;
 }
@@ -97,20 +46,20 @@ u_int32_t parse_http_host(const unsigned char *http_payload, const int payload_l
 	second_line_end = str_payload.find("\r\n", first_line_end+2);
 
 	std::string host = str_payload.substr(second_line_start, second_line_end - second_line_start);
-	std::cout << host << std::endl;
 
 	if (host.compare(host_filter) == 0) {
 		printf("   malicious host!!!\n");
+		printf("   malicious host name : %s\n", host_name);
 		return 0;
 	}
-	else 
-		return 1;
+	
+	return 1;
 }
 
 
 u_int32_t check_url (struct nfq_data *tb) 
 {
-	printf("in check_url function!!\n");
+	printf("in check_url function===============\n");
 	struct nfqnl_msg_packet_hdr *ph;
 	unsigned char *data;
 
@@ -146,15 +95,18 @@ u_int32_t check_url (struct nfq_data *tb)
 	int tcp_head_size = tcp_header->th_off * 4;
 	payload_len -= tcp_head_size;
 
+	// get total length of the packet
+	if (payload_len < 0) {
+		printf("not receive payload\n");
+		return 1;
+	}
+	
 	// http request
 	if (ntohs(tcp_header->th_dport) == PORT_HTTP) {
-		// unsigned char host[256] = "test.gilgil.net";
-		// int host_len = strlen((char *) host);
 		int host_len = strlen((char *) malicious_host);
 
 		// host url 비교
 		printf("HTTP Request\n");
-		// dump(data + ip_head_size + tcp_head_size, payload_len);
 		return parse_http_host(data+ip_head_size+tcp_head_size, payload_len, malicious_host, host_len);
 	}
 	// http response
@@ -168,11 +120,9 @@ u_int32_t check_url (struct nfq_data *tb)
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 	      struct nfq_data *nfa, void *data)
 {
-	printf("entering callback\n");
+	printf("entering callback===================\n");
 
-	printf("call print_pkt\n");
 	u_int32_t id = print_pkt(nfa);
-	printf("call check_url\n");
 	u_int32_t permission = check_url(nfa);
 	/*
 	 argument : uint32_t verdict
@@ -243,7 +193,7 @@ int main(int argc, char **argv)
 
 	for (;;) {
 		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
-			printf("pkt received\n");
+			printf("\npkt received\n");
 			nfq_handle_packet(h, buf, rv);
 			continue;
 		}
